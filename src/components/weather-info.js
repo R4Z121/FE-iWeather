c96028bf0dbeeb07103cc26cd91274d37f5b5a22
R4Z121 <m.rizkyazizi@gmail.com>
2022-12-11 10:40:58 +0700

    perbaikan kode weather-info.js dari line 11

--- src/components/weather-info.js
+++ src/components/weather-info.js
@@ -1,30 +1,24 @@
 /* eslint-disable default-case */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherInfo(props) {
   const Geocode = props.geocode;
+  const [locationAddress, setLocationAddress] = useState('');
+
   Geocode.fromLatLng(props.lat, props.lng).then(
     (response) => {
-      //const address = response.results[0].formatted_address;
-      let street, route, regency, city, state, country;
-
-      street = response.results[0].address_components[0].long_name;
-      route = response.results[0].address_components[1].long_name;
-      regency = response.results[0].address_components[2].long_name;
-      city = response.results[0].address_components[3].long_name;
-      state = response.results[0].address_components[4].long_name;
-      country = response.results[0].address_components[5].long_name;
-
-      document.getElementById("lokasi").innerHTML = street + ", " + regency + ", " + state;
-
+      const [
+        { long_name: street }, ,
+        { long_name: regency }, ,
+        { long_name: state }
+      ] = response.results[0].address_components
+      setLocationAddress(`${street}, ${regency}, ${state}`);
     }
-  ).catch(err => {
-    alert('Ga ada bang');
-  });
+  );
   return (
     <div id="card" className="flex justify-between rounded-md p-3 sm:p-5 w-full bg-app-black text-white gap-3">
-      <h1 className="text-base sm:text-xl w-2/3 font-bold" id="lokasi">Address, Location</h1>
+      <h1 className="text-base sm:text-xl w-2/3 font-bold" id="lokasi">{locationAddress}</h1>
       <div className="flex gap-1 p-1">
         <img
           src={`${process.env.PUBLIC_URL}/img/clear.png`}

e74932270291d4dd1f2f9ab3402cd458106a0b43
onirutlA <alturino001@gmail.com>
2022-12-12 10:01:42 +0700

    add: delete function at myReports.js

--- src/pages/myReports.js
+++ src/pages/myReports.js
@@ -1,23 +1,31 @@
-import React, { useState } from 'react'
-import AppBar from '../components/app-bar'
-import WeatherCard from '../components/weather-card'
-import AppFooter from '../components/app-footer'
-import ModalForm from '../components/modal-form'
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import AppBar from '../components/app-bar';
+import AppFooter from '../components/app-footer';
+import ModalForm from '../components/modal-form';
+import WeatherCard from '../components/weather-card';
+import { db } from "../firebase-config";
 
 export default function MyReports() {
+  const [info, setInformation] = useState([]);
   const [isModalShown, setIsModalShown] = useState(false);
+  const usersCollectionRef = collection(db, "weather-report");
+
+  const getUsers = async () => {
+    const data = await getDocs(usersCollectionRef);
+    const users = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    setInformation(users);
+    return users;
+  };
+
+  useEffect(() => {
+    getUsers();
+  });
+  //done here
 
   //get reports from user with userID = userID in localStorage
   const { id: userId } = JSON.parse(localStorage.getItem('user'));
-  //select * from weatherReport where userID = userId;
-  const info = {
-    reportedUser: 'Unknown',
-    address: 'West Midlands, Brighton',
-    weather: 'heavy_rain',
-    temperatures: '16',
-    reportedDate: '2022-12-6',
-    reportedTime: '14:53:23'
-  }
+  
 
   const addButtonClickHandler = () => {
     setIsModalShown(true);
@@ -35,7 +43,7 @@ export default function MyReports() {
           <span className='p-2 bg-app-black text-white rounded-md cursor-pointer' role='button' tabIndex='0' aria-label='active form modal' onClick={addButtonClickHandler}>Add Report</span>
         </section>
         <section className='p-3 w-full grid grid-cols-3'>
-          <WeatherCard info={info} isDeleteEnabled></WeatherCard>
+          { info.map((data, index) => <WeatherCard key={ index } info={ data } id={ data.id } isDeleteEnabled></WeatherCard>) }
         </section>
       </main>
       <AppFooter></AppFooter>
